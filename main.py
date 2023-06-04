6c22077660b3adac7d6c33ea28806b2bd12bd300
Kelvin <Kelzjr56@gmail.com>
2023-06-04 19:01:24 -0400

    Implemented validate method for CreditCard class, checks if payment is True before booking

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 # Read csv and use 'dtype' to convert csv number values to actual strings
 df = pd.read_csv('hotels.csv', dtype={"id": str})
+df_card = pd.read_csv('cards.csv', dtype=str).to_dict(orient="records")
 
 
 # Hotel class
@@ -42,16 +43,33 @@ class ReservationConfirmation:
         return content
 
 
+class CreditCard:
+    def __init__(self, number):
+        self.number = number
+
+    def validate(self, expiration, holder, cvc):
+        card_data = {"number": self.number, "expiration": expiration, "holder": holder, "cvc": cvc}
+
+        if card_data in df_card:
+            return True
+        else:
+            return False
+
+
 if __name__ == "__main__":
     print(df)
     hotel_ID = input("Enter an id of the hotel you want to book: ")
     hotel = Hotel(hotel_ID)
 
     if hotel.available():
-        hotel.book()
-        name = input("Enter your name: ")
-        confirm = ReservationConfirmation(customer_name=name, hotel_object=hotel)
-        print(confirm.generate())
+        credit_card = CreditCard(number="12345612345612")
+        if credit_card.validate(expiration="12/26", holder="JOHN SMITH", cvc="123"):
+            hotel.book()
+            name = input("Enter your name: ")
+            confirm = ReservationConfirmation(customer_name=name, hotel_object=hotel)
+            print(confirm.generate())
+        else:
+            print("There was a problem with your payment..")
 
     else:
         print("Sorry hotel is not free")
