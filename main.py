fb30a5e86d65ae999cbca06867665386a52e09fb
Kelvin <Kelzjr56@gmail.com>
2023-06-04 16:07:59 -0400

    Implemented Reservation class method to generate booking details to user

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ df = pd.read_csv('hotels.csv', dtype={"id": str})
 class Hotel:
     def __init__(self, local_hotel_id):
         self.local_hotel_id = local_hotel_id
+        self.name = df.loc[df['id'] == self.local_hotel_id, 'name'].squeeze()
 
     # Use row of dataframe column 'id' to assign value of 'no' to availability
     def book(self):
@@ -28,20 +29,28 @@ class Hotel:
 # Reservation Confirmation class
 class ReservationConfirmation:
     def __init__(self, customer_name, hotel_object):
-        pass
+        self.customer_name = customer_name
+        self.hotel_name = hotel_object
 
     def generate(self):
-        pass
+        content = f"""
+        Thank you for your reservation!
+        Here is your confirmation details:
+        Name: {self.customer_name}
+        Hotel name: {self.hotel_name.name}
+        """
+        return content
 
 
 if __name__ == "__main__":
     print(df)
-    hotel_id = input("Enter an id of the hotel you want to book: ")
-    hotel = Hotel(hotel_id)
+    hotel_ID = input("Enter an id of the hotel you want to book: ")
+    hotel = Hotel(hotel_ID)
+
     if hotel.available():
         hotel.book()
         name = input("Enter your name: ")
-        confirm = ReservationConfirmation(name, hotel)
+        confirm = ReservationConfirmation(customer_name=name, hotel_object=hotel)
         print(confirm.generate())
 
     else:
